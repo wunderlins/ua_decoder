53b52a30edc25610accc1d4ce71ae3fbb253cb9d
wus <swunderlin@gmail.com>
2015-05-06 19:10:52 +0200

    env

--- env.sh
+++ env.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
-export basedir=`dirname ${BASH_SOURCE[0]}`"/.."
+basedir=`dirname ${BASH_SOURCE[0]}`
+export PATH="$PATH:$basedir"
+#echo "basedir: $basedir"
+#echo $PATH
 
-if [[ $(which realpath) != 0 ]]; then
+which realpath >/dev/null 2>&1
+if [[ $? != 0 ]]; then
 	cat <<- EOT
 
-Error, realpath required.
+Error, realpath required, not found.
 
-Your package manager might provide a way to install the realpath utility. If 
-not, you can get an C source implementation here:
+Make sure it's location is in set your \$PATH environment variable.
+
+If the utility is not installed, your package manager might provide a way to 
+install the realpath utility. If not, you can get an C source implementation 
+here:
 http://stackoverflow.com/questions/284662/how-do-you-normalize-a-file-path-in-bash#284664
 
 Installation of realpath
 * Download the file, save it as realpath.c inside the project directory
 * make sure you have a C compiler (apt-get install build-essential on debian)
 * compile it with $ gcc -o realpath realpath.c
-* make sure realpath is in your PATH, ie. export PATH=./:$PATH
+* make sure realpath is in your PATH, ie. export PATH=./:\$PATH
 
 EOT
 
+#echo $PATH | grep '\./:' >/dev/null; 
 return 1
 fi
 
-basedir=`$basedir/bin/realpath $basedir`
+export basedir=`realpath $basedir`
 
-PS1="<\[\033[31m\]\h\[\033[0m\]:\[\033[36m\]$server\[\033[0m\]> \[\033[34m\]\W\[\033[0m\]\[\033[37m\]%\[\033[0m\] "
+PS1="<\[\033[37m\]\u\[\033[0m\]@\[\033[31m\]\h\[\033[0m\]> \[\033[33m\]\W\[\033[0m\]\[\033[36m\]%\[\033[0m\] "
